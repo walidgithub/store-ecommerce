91d69fe7bf4cb3a3252d87443df16d52550dd792
walidgithub <leadermatrix2@yahoo.com>
2021-08-22 07:51:03 +0200

    video 8

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,6 +46,15 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+                Route::middleware('web')
+                ->namespace($this->namespace)
+                ->prefix('admin')
+                ->group(base_path('routes/admin.php'));
+
+                Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/site.php'));
         });
     }
 
---
+++ routes/admin.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['namespace' => 'Dashboard', 'middleware' => 'auth:admin'],function(){
+
+    Route::get('users',function(){
+return 'in admin';
+    });
+
+});
